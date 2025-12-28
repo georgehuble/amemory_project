45995f9bf030c60495439d21a1259c109995f45a
horhe <jorhe@MacBook-Pro-Georgij.local>
2025-12-28 23:16:40 +0300

    fix

--- vite.config.js
+++ vite.config.js
@@ -2,9 +2,9 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 export default defineConfig({
-  base: '/', // Просто корень
+  base: '/',
   plugins: [vue()],
   build: {
-    outDir: 'docs'
+    outDir: 'dist'
   }
 })

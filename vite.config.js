8d7d2b035e377456f577423b195bf79d2156bd8d
horhe <jorhe@MacBook-Pro-Georgij.local>
2025-12-28 23:01:59 +0300

    fix

--- vite.config.js
+++ vite.config.js
@@ -2,27 +2,9 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 export default defineConfig({
-  base: '/amemory_project/', // Важно для GitHub Pages
+  base: '/', // Просто корень
   plugins: [vue()],
   build: {
-    outDir: 'docs',
-    assetsDir: 'assets', // Все ассеты будут в docs/assets/
-    rollupOptions: {
-      input: {
-        main: './index.html'
-      },
-      output: {
-        assetFileNames: (assetInfo) => {
-          if (assetInfo.name === 'index.html') return '[name].[ext]'
-          return 'assets/[name]-[hash][extname]'
-        }
-      }
-    }
-  },
-  // Для корректной обработки путей
-  resolve: {
-    alias: {
-      '@': '/src'
-    }
+    outDir: 'docs'
   }
 })

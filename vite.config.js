6ebd06fa4816f6221351e20f879ccf21172314a4
horhe <jorhe@MacBook-Pro-Georgij.local>
2025-12-28 22:47:17 +0300

    fix

--- vite.config.js
+++ vite.config.js
@@ -1,22 +1,10 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  base: process.env.NODE_ENV === 'production' ? '/amemory_project/' : '/',
+  base: './', // Важно! Относительный путь
   plugins: [vue()],
-  server: {
-    port: 3000,
-    open: true // автоматически открывает браузер при запуске dev сервера
-  },
   build: {
-    outDir: 'docs',
-    emptyOutDir: true,
-    // Оптимизация для GitHub Pages
-    rollupOptions: {
-      output: {
-        manualChunks: undefined
-      }
-    }
+    outDir: 'docs'
   }
 })

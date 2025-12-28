6648bf5fdacbe53cce36f99b8a0c4b32609584d0
horhe <jorhe@MacBook-Pro-Georgij.local>
2025-12-28 21:46:59 +0300

    remote fix

--- vite.config.js
+++ vite.config.js
@@ -10,7 +10,7 @@ export default defineConfig({
     open: true // автоматически открывает браузер при запуске dev сервера
   },
   build: {
-    outDir: 'dist',
+    outDir: 'docs',
     emptyOutDir: true,
     // Оптимизация для GitHub Pages
     rollupOptions: {

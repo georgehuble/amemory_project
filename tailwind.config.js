9ae1e3b90d3b3e3bf378a6d7e03c92cd4910b1e8
jorhe <jorhe@Jorhe.lan>
2025-12-29 16:09:01 +0300

    refactor

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./*.{html,js}", "./src/*/.{html,js}"],
+  content: ["./*.{html,js}", "./assets/**/*.js", "./components/**/*.html", "./utils/**/*.js"],
   theme: {
     extend: {
       colors: {
--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,29 @@
 import { defineConfig } from 'vite'
 import { createVuePlugin } from 'vite-plugin-vue2'
+import { resolve } from 'path'
 
 export default defineConfig({
   base: '/',
   plugins: [createVuePlugin()],
   build: {
     outDir: 'dist',
-    emptyOutDir: true
+    emptyOutDir: true,
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        about: resolve(__dirname, 'about.html'),
+        blog: resolve(__dirname, 'blog.html'),
+        contact: resolve(__dirname, 'contact.html'),
+        dashboard: resolve(__dirname, 'dashboard.html'),
+        login: resolve(__dirname, 'login.html'),
+        registration: resolve(__dirname, 'registration.html'),
+        single: resolve(__dirname, 'single.html'),
+        testimonial: resolve(__dirname, 'testimonial.html'),
+        forgotPassword: resolve(__dirname, 'forgot-password.html'),
+        resetPassword: resolve(__dirname, 'reset-password.html'),
+        verifyCode: resolve(__dirname, 'verify-code.html'),
+        spinningWheels: resolve(__dirname, 'spinning-wheels-full.html')
+      }
+    }
   }
 })
